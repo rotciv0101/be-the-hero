dbd59aaf5a654353722e4b71bb75f4aef5074114
rotciv0101 <victorgabriel1730@gmail.com>
2020-03-29 21:36:26 -0300

    mudanças e procfile

--- Back-end/src/index.js
+++ Back-end/src/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const {errors} = require('celebrate');
+
 const cors = require('cors');
 
 const routes = require('./routes');
@@ -9,6 +11,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes); 
-
+app.use(errors());
 
 app.listen(3333);
